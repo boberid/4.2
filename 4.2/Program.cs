f4ea9a5a82ea6344f13762d37ec56e4852d6dc65
Алёна Тимофеева <alyona.timofeeva.04@mail.ru>
2022-02-01 11:05:38 +0500

    Добавьте файлы проекта.

---
+++ 4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            Console.WriteLine("N = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[N];
+            double a = 2;
+            for (int i = 1; i <= N; i++)
+            {
+                double y = Math.Pow(a, i);
+                Console.WriteLine(i + "\t" + y);
+            }
+        }
+    }
+}
